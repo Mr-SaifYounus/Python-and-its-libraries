82bd0368f81d6054806f8c5b75a6251a808b9fe0
Mr-SaifYounus <engr.saifyounus@gmail.com>
2024-09-25 19:33:21 -0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+square_dict={x:x**3 for x in range(1,6)}
+print(square_dict)
+print("="*50)
+square_dict1 = {}
+for x in range(1, 6):
+    square_dict1[x] = x**3
+print(square_dict1)
+
+
+ 
---
+++ Untitled-2.py
@@ -0,0 +1,9 @@
+row=1
+while row<10:
+    column=1
+    while column<10:
+        prod=row*column
+        print(row*column,"=",prod," ",end="")
+        column+=1
+print()
+row+=1
